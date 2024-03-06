a6144201ed7cbfc12bbee26d8eb312cd39a6466f
hamed <khaledihkh@gmail.com>
2024-03-06 15:52:05 +0330

    add main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ start_time = time.time()
 preprocessor = Preprocessor()
 deduplication = Deduplication()
 if __name__ == '__main__':
-    preprocessor.preprocess_files('crawl', filtering=True)
-    deduplication.preprocess_files('crawl')
+    preprocessor.preprocess_files('SocialMedia', filtering=True)
+    deduplication.preprocess_files('SocialMedia')
     print("--- %s seconds ---" % (time.time() - start_time))
